0fbc43abecc7005976ebeeb596728be59475f084
Gary Deer <gdeer81@gmail.com>
2017-10-09 20:15:00 -0500

    bumped specter version and added answer key for new koans

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject specter-koans "0.1.0-SNAPSHOT"
+(defproject specter-koans "0.2.0"
   :description "Specter Koans"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.rpl/specter "0.13.0"]
-                  [koan-engine "0.2.3"]]
+                 [com.rpl/specter "1.0.3"]
+                 [koan-engine "0.2.3"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init ^:displace (do (use '[koan-engine.core]))}
--- resources/koans.clj
+++ resources/koans.clj
@@ -1,12 +1,97 @@
 [["01_selection" {"__" [true
-                     2
-		     5
-		     true
-		     false
-		     true
-		     true
-		     false
-		     "hello"
-		     "BOB"
-		     nil
-		     3]}]]
+                        2
+                        5
+                        true
+                        false
+                        true
+                        true
+                        false
+                        "hello"
+                        "BOB"
+                        nil
+                        3]}
+  "02_unparameterized_navigators" {"__" ["YES"
+                                         ["Peter" "Paul" "Mary"]
+                                         false
+                                         [0 1 2 3]
+                                         [2 4]
+                                         [[:a :b] [:c :d]]
+                                         {:name "Rich" :language-created "Clojure"}
+                                         [1 2 3]
+                                         42
+                                         [:i :before :e]
+                                         (range 0 11)
+                                         "unbelievable"
+                                         (range 0 11)
+                                         "unbelievable"
+                                         :peanuts
+                                         nil
+                                         [1 2 3]
+                                         \C
+                                         "Clojure"
+                                         [[0 1] [1 2] [2 3] [3 4] [4 5]]
+                                         [5 4 3 2 1]
+                                         [1 5 4 3 2]
+                                         4
+                                         [1 :b]
+                                         nil
+                                         [:a :b :c :d]
+                                         \c
+                                         "absee"
+                                         [:a :b]
+                                         [3 4]
+                                         {:a 1 :c 3}
+                                         [:c :f]
+                                         {:meta :data}
+                                         {:meta :datum}
+                                         ["key"]
+                                         ["a" "b" "c"]
+                                         {a/q 3, bbb/q 4, q 5}
+                                         ["koans.02-unparameterized-navigators" "koans.02-unparameterized-navigators"]
+                                         ["koans.02-unparameterized-navigators" nil]
+                                         [:a/test :a/fun]
+                                         '()
+                                         #{}
+                                         []
+                                         :some-val-other-than-nil
+                                         #{1 2 3}
+                                         #{1}
+                                         :dest-key
+                                         nil
+                                         []
+                                         [0 1 2 3 4]
+                                         [[(0 1 2) 0] [(0 1 2) 1] [(0 1 2) 2]]
+                                         [5 6 7 8 9]]}
+  "03_parameterized_navigators_and_functions" {"__" ["yes"
+                                                     [:a 1 2 3]
+                                                     :com.rpl.specter.impl/NONE
+                                                     [1 2 3]
+                                                     [1 :a 2 3]
+                                                     [1 2 3 :a]
+                                                     [{:a 2}]
+                                                     [[0 1 2] 0]
+                                                     [[[0 1 2] 0] [[0 1 2] 1] [[0 1 2] 2]]
+                                                     [[[0 1 2] [0 1 2] 0] [[0 1 2] [0 1 2] 1] [[0 1 2] [0 1 2] 2]]
+                                                     [6 1 2 3 4]
+                                                     '([0 1 2] 1 2)
+                                                     [0 4]
+                                                     [[0 0] [0 1] [0 2]]
+                                                     {:a 5 :b 3}
+                                                     {:a 105 :b 5 :c 7}
+                                                     0
+                                                     42
+                                                     [0 2]
+                                                     []
+                                                     '(0 1 2 {:a 0, :b 1, :c 2})
+                                                     '([5 6] [3] [2 5])
+                                                     []
+                                                     [11 12 20]
+                                                     "yes"
+                                                     [() [] #{} {} "" true]
+                                                     [0 2 4 6 8]
+                                                     '(0)
+                                                     '(1)
+                                                     '()
+                                                     [0]
+                                                     []
+                                                     [3 1 2 4 5]]}]]
